c9dcbf0fb19da3920c02603614c00cd78a51e37a
Thomas59 <thomas.lemay59@outlook.fr>
2023-01-16 12:42:59 +0100

    fonction affichage grille

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+def afficher_grille(plateau:dict) -> None:
+    """Fonction qui affiche la grille du morpion
+    Args:
+        plateau (dict): Un plateau de jeu
+    """
+    os.system("clear")
+    print(" \t|\t0\t|\t1\t|\t2\t|")
+    print("---------------------------------------------------------")
+    for cle in plateau:
+        print(cle, end="\t|\t")
+        for elt in plateau[cle]:
+            if elt == None:
+                print(" ", end="\t|\t")
+            else:
+                print(elt, end="\t|\t")
+        print("\n---------------------------------------------------------")
