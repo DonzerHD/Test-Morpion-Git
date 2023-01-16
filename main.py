2604ee534c0068828ec6db1a9fd4cecdf2a3debf
Ingabire Yvette <johndoe@example.com>
2023-01-16 13:50:46 +0100

    fonction coup_valide

--- main.py
+++ main.py
@@ -15,3 +15,26 @@ def afficher_grille(plateau:dict) -> None:
             else:
                 print(elt, end="\t|\t")
         print("\n---------------------------------------------------------")
+
+def est_coup_valide(plateau:dict, coup:str) -> bool:
+    """Fonction qui vérifie si un coup est valide
+
+    Args:
+        plateau (dict): Le plateau de jeu
+        coup (str): Un coup de la forme "A1" avec le premier caractere entre A et C et le second entre 0 et 2 
+
+    Returns:
+        bool: True si le coup est valide, False sinon
+    """
+    
+    # On vérifie si le coup est une case qui existe
+    if coup[0].upper() not in ["A", "B", "C"]:
+        return False
+    if coup[1] not in ["0", "1", "2"]:
+        return False
+    
+    # On vérifie si la case est vide
+    if plateau[coup[0].upper()][int(coup[1])] == None:
+        return True
+    else:
+        return False
